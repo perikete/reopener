600fd27cb65f6f90366131e70cccfce87b334cbe
Juan Pedro <perikete@gmail.com>
2016-07-26 15:50:36 +1200

    Building tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,38 @@
-/**
- * Created by jelizalde on 26/07/2016.
- */
+'use strict';
+
+var gulp = require('gulp');
+var Uglify = require('gulp-uglify');
+
+gulp.task('default', function (done) {
+
+    gulp.src(['./node_modules/jquery/dist/jquery.min.js'])
+        .pipe(gulp.dest('./js'));
+
+    gulp.src(['./node_modules/bootstrap/dist/css/bootstrap.min.css'])
+        .pipe(gulp.dest('./css'));
+
+    done();
+});
+
+gulp.task('build', ['default'], function (done) {
+
+    gulp.src([
+        '*css/**/*',
+        './logo.png',
+        './manifest.json',
+        './options.html'])
+        .pipe(gulp.dest('./dist'));
+
+    gulp.src(['./js/options.js'])
+        .pipe(Uglify())
+        .pipe(gulp.dest('./dist/js'));
+
+    gulp.src(['./js/jquery.min.js'])
+        .pipe(gulp.dest('./dist/js'));
+
+    gulp.src(['./js/background.js'])
+        .pipe(Uglify())
+        .pipe(gulp.dest('./dist/js'));
+
+    done();
+});
